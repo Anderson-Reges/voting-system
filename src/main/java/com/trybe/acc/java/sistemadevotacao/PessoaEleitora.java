42cccb8c5fadee19cab13e3e51b3c7c530fe83ec
Anderson-Reges <andersonreges76@gmail.com>
2023-09-12 16:50:41 -0300

    adicionando as estruturas das classes PessoaEleitora

--- src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
+++ src/main/java/com/trybe/acc/java/sistemadevotacao/PessoaEleitora.java
@@ -1,5 +1,29 @@
 package com.trybe.acc.java.sistemadevotacao;
 
-public class PessoaEleitora {
+public class PessoaEleitora extends Pessoa{
 
+  private String cpf;
+
+  PessoaEleitora(String nome, String cpf) {
+    this.cpf = cpf;
+    this.nome = nome;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  @Override
+  public String getNome() {
+    return this.nome;
+  }
+
+  @Override
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
 }
